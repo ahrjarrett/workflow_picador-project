f1d1e08629d4c16605499dc02b2769c3a70c0c01
Andrew Jarrett <aj@Andrews-MacBook-Pro.local>
2016-01-19 10:48:35 -0600

    added gulp tasks sans coffee

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'js', 'compass', 'connect', 'watch']);
